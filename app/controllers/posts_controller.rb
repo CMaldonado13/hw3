996daacb62f405b4e12930293c601681a1514874
CMaldonado13 <christian.maldonado1@kellogg.northwestern.edu>
2023-02-21 15:43:58 +0000

    built places

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,46 @@
 class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+      end
+    
+      def show
+        @post = Post.find_by({ "id" => params["id"] })
+        @place = Place.where({ "id" => @post["place_id"] })
+      end
+    
+      def new
+        @post = Post.new
+        @place = Place.find_by({"id" => params["place_id"]})
+        @post["place_id"] = @place["id"]
+      end
+    
+      def create
+        @post = Post.new
+        @post["title"] = params["post"]["title"]
+        @post["description"] = params["post"]["description"]
+        @post["posted_on"] = params["post"]["posted_on"]
+        @post["place_id"] = params["post"]["place_id"]
+        @post.save
+        redirect_to "/posts"
+      end
+    
+      def edit
+        @post = Post.find_by({ "id" => params["id"] })
+      end
+    
+      def update
+        @post = Post.find_by({ "id" => params["id"] })
+        @post["title"] = params["post"]["title"]
+        @post["description"] = params["post"]["description"]
+        @post["posted_on"] = params["post"]["posted_on"]
+        @post["place_id"] = params["post"]["place_id"]
+        @post.save
+        redirect_to "/posts"
+      end
+    
+      def destroy
+        @post = Post.find_by({ "id" => params["id"] })
+        @post.destroy
+        redirect_to "/posts"
+      end
 end
